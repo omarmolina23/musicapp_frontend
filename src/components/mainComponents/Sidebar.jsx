d04e2aa687857c59903993c1fc9c7949b160d516
Omar <omardavidjm@ufps.edu.co>
2025-01-30 23:17:32 -0500

    feat: add sidebar and music player components to main layout

---
+++ src/components/mainComponents/Sidebar.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+import { SideButton } from "./SideButton";
+export function Sidebar() {
+  return (
+      <div className="bg-slate-900 h-[575px] w-[400px] ml-2 mr-3 rounded-md">
+        <div style={{ display: 'flex', flexDirection: 'row', overflowX: 'auto' }}>
+        <SideButton label="Listas" active={false} />
+        <SideButton label="Artistas" active={false} />
+        <SideButton label="Álbumes" active={false} />
+        <SideButton label="Álbumes" active={false} />
+        </div>
+        
+      </div>
+    );
+  
+}
